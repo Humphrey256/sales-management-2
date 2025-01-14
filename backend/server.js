8def62e10465c5b985c7cfe8a0d246f2addfc54e
Humphrey256 <muronhumfix@gmail.com>
2025-01-14 19:47:27 +0300

    serving the frontend from the backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const salesRoutes = require('./routes/salesRoutes');
 const dbConnect = require('./config/db');
 const cors = require('cors');
-const path = require('path');  // Import path module to work with file paths
+const path = require('path'); // Import path module to work with file paths
 require('dotenv').config();
 
 const app = express();
@@ -11,29 +11,29 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://sales-management-yd7n.onrender.com', 'http://localhost:3000'], // Allow front-end origins
+  origin: ['http://localhost:3000'], // Allow front-end origins
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));  // Enable CORS with the frontend URLs
+})); // Enable CORS with the frontend URLs
 app.use(express.json());
 
 // Database Connection
 dbConnect();
 
 // Basic route to verify the server is up
-app.get('/', (req, res) => {
-  res.send('Backend is running');
+app.get('/api', (req, res) => {
+  res.send('Backend API is running');
 });
 
 // API Routes
 app.use('/api/sales', salesRoutes);
 
 // Serve React App's static files from the 'build' folder
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Catch-all route to send index.html for all non-API requests (to allow React Router to handle routing)
+// Fallback route to serve index.html for React Router on any non-API request
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
